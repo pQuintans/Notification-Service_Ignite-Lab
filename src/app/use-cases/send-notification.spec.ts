277862049b17706d75930a915261ae4124a9960d
pQuintans <caixote.dom@gmail.com>
2022-12-26 19:53:06 -0300

    Finish app v1

---
+++ src/app/use-cases/send-notification.spec.ts
@@ -0,0 +1,18 @@
+import { InMemoryNotificationRepository } from '../../../test/repositories/in-memory-notifications-repository';
+import { SendNotification } from './send-notification';
+
+describe('Send notification', () => {
+  it('should be able to send a notification', async () => {
+    const notificationRepository = new InMemoryNotificationRepository();
+    const sendNotification = new SendNotification(notificationRepository);
+
+    const { notification } = await sendNotification.execute({
+      recipientId: 'example-recipient-id',
+      content: 'Você recebeu uma solicitação de amizade',
+      category: 'social',
+    });
+
+    expect(notificationRepository.notifications).toHaveLength(1);
+    expect(notificationRepository.notifications[0]).toEqual(notification);
+  });
+});
